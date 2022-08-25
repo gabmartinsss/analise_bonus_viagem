24b4c50a58dad2348e1b21f11ef6eed7e4810e84
Gabrielle Martins <gabi_maartins@hotmail.com>
2022-08-25 00:08:30 -0300

    Localizar colaborador que bateu a meta

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mes in lista_meses:
-    print(mes)
     tabela_vendas = pd.read_excel(f'{mes}.xlsx')
-    print(tabela_vendas)
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês {mes} alguém bateu a meta!')
+        print(f'Vendedor: {vendedor} | Vendas: R${vendas}')
